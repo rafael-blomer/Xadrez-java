87eeb7f706db36a9c5ef8b48f5092df637176098
Abc Bolinhas <abc@bolinhas.com>
2024-06-26 15:07:44 -0300

    exceção xadrez e posição xadrez

--- src/chess/PartidaXadrez.java
+++ src/chess/PartidaXadrez.java
@@ -12,9 +12,9 @@ public class PartidaXadrez {
 		tabuleiro = new Tabuleiro(8, 8);
 		setupInicial();
 	}
-	
+
 	public PecaXadrez[][] getPecas() {
-		PecaXadrez[][] mat  = new PecaXadrez[tabuleiro.getRows()][tabuleiro.getColumns()];
+		PecaXadrez[][] mat = new PecaXadrez[tabuleiro.getRows()][tabuleiro.getColumns()];
 		for (int i = 0; i < tabuleiro.getRows(); i++) {
 			for (int j = 0; j < tabuleiro.getColumns(); j++) {
 				mat[i][j] = (PecaXadrez) tabuleiro.peca(i, j);
@@ -22,10 +22,14 @@ public class PartidaXadrez {
 		}
 		return mat;
 	}
-	
+
+	private void placeNewPiece(char column, int row, PecaXadrez piece) {
+		tabuleiro.colocarPeca(piece, new PosicaoXadrez(column, row).toPosition());
+	}
+
 	private void setupInicial() {
-		tabuleiro.colocarPeca(new Rook(tabuleiro, Cor.WHITE), new Posicao(2, 1));
-		tabuleiro.colocarPeca(new King(tabuleiro, Cor.BLACK), new Posicao(0, 4));
-		tabuleiro.colocarPeca(new King(tabuleiro, Cor.WHITE), new Posicao(7, 4));
+		placeNewPiece('b', 6 ,new Rook(tabuleiro, Cor.WHITE));
+		placeNewPiece('e', 8, new King(tabuleiro, Cor.BLACK));
+		placeNewPiece('e', 1,new King(tabuleiro, Cor.WHITE));
 	}
 }
---
+++ src/chess/PosicaoXadrez.java
@@ -0,0 +1,36 @@
+package chess;
+
+import boardgame.Posicao;
+
+public class PosicaoXadrez {
+	private char column;
+	private int row;
+
+	public PosicaoXadrez(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8)
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	
+	protected Posicao toPosition() {
+		return new Posicao(8 - row, column - 'a');
+	}
+	
+	protected static PosicaoXadrez fromPosition(Posicao posicao) {
+		return new PosicaoXadrez((char)('a' - posicao.getColumn()), 8 - posicao.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+}
